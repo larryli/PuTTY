3992fc48f11ffed290ffe6f1cdff61d63e4fc990
Simon Tatham <anakin@pobox.com>
1999-10-25 14:27:13 +0000

    Fix versioncmp
    [originally from svn r255]

--- ssh.c
+++ ssh.c
@@ -217,13 +217,13 @@ static int ssh_versioncmp(char *a, char *b) {
     char *ae, *be;
     unsigned long av, bv;
 
-    av = strtoul(a, &ae);
-    bv = strtoul(b, &be);
+    av = strtoul(a, &ae, 10);
+    bv = strtoul(b, &be, 10);
     if (av != bv) return (av < bv ? -1 : +1);
     if (*ae == '.') ae++;
     if (*be == '.') be++;
-    av = strtoul(ae, &ae);
-    bv = strtoul(be, &be);
+    av = strtoul(ae, &ae, 10);
+    bv = strtoul(be, &be, 10);
     if (av != bv) return (av < bv ? -1 : +1);
     return 0;
 }
