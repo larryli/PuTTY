8e5e0225e597001c55637fb4ba53839707d465f8
Simon Tatham <anakin@pobox.com>
2001-05-14 13:31:14 +0000

    Scrollbar was failing to update when no scrollback-reset event had happened
    [originally from svn r1126]

--- terminal.c
+++ terminal.c
@@ -298,11 +298,12 @@ void term_update(void)
     Context ctx;
     ctx = get_ctx();
     if (ctx) {
+	if (seen_disp_event)
+	    update_sbar();
 	if ((seen_key_event && (cfg.scroll_on_key)) ||
 	    (seen_disp_event && (cfg.scroll_on_disp))) {
 	    disptop = 0;	       /* return to main screen */
 	    seen_disp_event = seen_key_event = 0;
-	    update_sbar();
 	}
 	do_paint(ctx, TRUE);
 	sys_cursor(curs.x, curs.y - disptop);
