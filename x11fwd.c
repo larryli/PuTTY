f4e13ef19b63ad784070cf194795523cf327ebae
Ben Harris <bjh21@bjh21.me.uk>
2003-01-05 14:23:30 +0000

    "possible unintended assignment"?  I think not.
    [originally from svn r2468]

--- x11fwd.c
+++ x11fwd.c
@@ -204,7 +204,7 @@ char *x11_init(Socket * s, char *display, void *c, void *auth)
      * Try to find host.
      */
     addr = name_lookup(host, port, &dummy_realhost);
-    if ((err = sk_addr_error(addr)))
+    if ((err = sk_addr_error(addr)) != NULL)
 	return err;
 
     port = 6000 + displaynum;
@@ -222,7 +222,7 @@ char *x11_init(Socket * s, char *display, void *c, void *auth)
     pr->c = c;
 
     pr->s = *s = new_connection(addr, dummy_realhost, port, 0, 1, 0, (Plug) pr);
-    if ((err = sk_socket_error(*s))) {
+    if ((err = sk_socket_error(*s)) != NULL) {
 	sfree(pr);
 	return err;
     }
