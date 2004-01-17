6d7cc86470782937ea60a468a64fa661be21d673
Simon Tatham <anakin@pobox.com>
2004-01-17 14:17:21 +0000

    Building source archives for previous releases has always been a fiddly process. Let's have a magic script designed to do it right.
    [originally from svn r3722]

---
+++ mksrcarc.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+perl mkfiles.pl
+relver=`cat LATEST.VER`
+unixarc="putty-$relver$datesuffix"
+text=`{ find . -name CVS -prune -o \
+               -name .cvsignore -prune -o \
+               -name LATEST.VER -prune -o \
+               -name CHECKLST.txt -prune -o \
+               -name mksrcarc.sh -prune -o \
+               -name '*.dsp' -prune -o \
+               -name '*.dsw' -prune -o \
+               -type f -print | sed 's/^\.\///'; } | \
+      grep -ivE MODULE\|putty.iss\|website.url | grep -vF .ico`
+bin=`{ ls -1 *.ico putty.iss website.url; \
+       find . -name '*.dsp' -print -o -name '*.dsw' -print; }`
+zip -k -l putty-src.zip $text > /dev/null
+zip -k putty-src.zip $bin > /dev/null
