ab659aac49d42dbad505bd6a34556df2ec94ec7f
Simon Tatham <anakin@pobox.com>
2005-03-11 09:07:37 +0000

    Fiddle with source archive for new OS X files.
    [originally from svn r5489]

--- mksrcarc.sh
+++ mksrcarc.sh
@@ -10,14 +10,14 @@ text=`{ find . -name CVS -prune -o \
                -name '*.dsp' -prune -o \
                -name '*.dsw' -prune -o \
                -type f -print | sed 's/^\.\///'; } | \
-      grep -ivE 'testdata/.*\.txt|MODULE|putty.iss|website.url' | grep -vF .ico`
+      grep -ivE 'testdata/.*\.txt|MODULE|putty.iss|website.url' | grep -vF .ico | grep -vF .icns`
 # These are files which I'm _sure_ should be treated as text, but
 # which zip might complain about, so we direct its moans to
 # /dev/null! Apparently its heuristics are doubtful of UTF-8 text
 # files.
 bintext=testdata/*.txt
 # These are actual binary files which we don't want transforming.
-bin=`{ ls -1 windows/*.ico windows/putty.iss windows/website.url; \
+bin=`{ ls -1 windows/*.ico windows/putty.iss windows/website.url macosx/*.icns; \
        find . -name '*.dsp' -print -o -name '*.dsw' -print; }`
 zip -k -l putty-src.zip $text > /dev/null
 zip -k -l putty-src.zip $bintext >& /dev/null
