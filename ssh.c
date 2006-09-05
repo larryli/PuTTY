2afdb0266036e0391b93862850f9ba9fb8efa6d0
Jacob Nevins <jacobn@chiark.greenend.org.uk>
2006-09-05 21:41:38 +0000

    I seem to have slightly funted Plink's display of banners etc in r6437, oops. Fixed.
    [originally from svn r6846]
    [r6437 == 8719f92c1426609de7ead4a490a15d1d18875f53]

--- ssh.c
+++ ssh.c
@@ -1052,7 +1052,7 @@ static void c_write_stderr(int trusted, const char *buf, int len)
 {
     int i;
     for (i = 0; i < len; i++)
-	if (buf[i] != '\r' && (trusted || buf[i] & 0x60))
+	if (buf[i] != '\r' && (trusted || buf[i] == '\n' || (buf[i] & 0x60)))
 	    fputc(buf[i], stderr);
 }
 
