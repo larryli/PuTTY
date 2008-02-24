9503c5e5c32902235d392d599fc9570e705c9144
Jacob Nevins <jacobn@chiark.greenend.org.uk>
2008-02-24 00:16:29 +0000

    It's a new year (and we've even made a code checkin).
    [originally from svn r7883]
    [this svn revision also touched putty-website]

--- unix/gtkdlg.c
+++ unix/gtkdlg.c
@@ -2544,7 +2544,7 @@ static void licence_clicked(GtkButton *button, gpointer data)
     char *title;
 
     char *licence =
-	"Copyright 1997-2007 Simon Tatham.\n\n"
+	"Copyright 1997-2008 Simon Tatham.\n\n"
 
 	"Portions copyright Robert de Bath, Joris van Rantwijk, Delian "
 	"Delchev, Andreas Schultz, Jeroen Massar, Wez Furlong, Nicolas "
@@ -2625,7 +2625,7 @@ void about_box(void *window)
 		       w, FALSE, FALSE, 5);
     gtk_widget_show(w);
 
-    w = gtk_label_new("Copyright 1997-2007 Simon Tatham. All rights reserved");
+    w = gtk_label_new("Copyright 1997-2008 Simon Tatham. All rights reserved");
     gtk_box_pack_start(GTK_BOX(GTK_DIALOG(aboutbox)->vbox),
 		       w, FALSE, FALSE, 5);
     gtk_widget_show(w);
