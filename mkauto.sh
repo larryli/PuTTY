9fe425c2810cf6026542624f2043ddff875451cd
Simon Tatham <anakin@pobox.com>
2008-04-02 17:09:53 +0000

    It's nice to be able to run mkauto.sh from inside the unix subdir as well as from outside.
    [originally from svn r7963]

--- mkauto.sh
+++ mkauto.sh
@@ -3,6 +3,10 @@
 # It's separate from mkfiles.pl because it won't work (and isn't needed)
 # on a non-Unix system.
 
+# It's nice to be able to run this from inside the unix subdir as
+# well as from outside.
+test -f unix.h && cd ..
+
 # Track down automake's copy of install-sh
 cp `aclocal --print-ac-dir | sed 's/aclocal$/automake/'`/install-sh unix/.
 (cd unix && autoreconf  && rm -rf aclocal.m4 autom4te.cache)
