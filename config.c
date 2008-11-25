larryli/PuTTY

commit a8825c6d840d3fb4ff5e4247d231960d9fad3cbe

Revamp of the local X11 connection code. We now parse X display strings more rigorously, and then we look up the local X authority data in .Xauthority _ourself_ rather than delegating to an external xauth program. This is (negligibly) more efficient on Unix, assuming I haven't got it wrong in some subtle way, but its major benefit is that we can now support X authority lookups on Windows as well provided the user points us at an appropriate X authority file in the standard format. A new Windows-specific config option has been added for this purpose.
git-svn-id: svn://svn.tartarus.org/sgt/putty@8305 cda61777-01e9-0310-a592-d414129be87e

windows/wincfg.c
@@ -377,4 +377,14 @@ void win_setup_config_box(struct controlbox *b, HWND *hwndp, int has_help,
      */
     if (!midsession || (protocol == PROT_SERIAL))
         ser_setup_config_box(b, midsession, 0x1F, 0x0F);
+
+    /*
+     * $XAUTHORITY is not reliable on Windows, so we provide a
+     * means to override it.
+     */
+    s = ctrl_getset(b, "Connection/SSH/X11", "x11", "X11 forwarding");
+    ctrl_filesel(s, "X authority file for local display", 't',
+		 NULL, FALSE, "Select X authority file",
+		 HELPCTX(ssh_tunnels_xauthority),
+		 dlg_stdfilesel_handler, I(offsetof(Config, xauthfile)));
 }

Recipe
@@ -302,10 +302,10 @@ U_BE_NOSSH = be_nos_s uxser nocproxy
 # keywords [G] for Windows GUI app, [C] for Console app, [X] for
 # X/GTK Unix app, [U] for command-line Unix app, [M] for Macintosh app.
 
-putty    : [G] GUITERM NONSSH WINSSH W_BE_ALL WINMISC putty.res LIBS
+putty    : [G] GUITERM NONSSH WINSSH W_BE_ALL WINMISC winx11 putty.res LIBS
 puttytel : [G] GUITERM NONSSH W_BE_NOSSH WINMISC puttytel.res LIBS
 plink    : [C] winplink wincons NONSSH WINSSH W_BE_ALL logging WINMISC
-         + plink.res LIBS
+         + winx11 plink.res LIBS
 pscp     : [C] pscp winsftp wincons WINSSH BE_SSH SFTP wildcard WINMISC
          + pscp.res LIBS
 psftp    : [C] psftp winsftp wincons WINSSH BE_SSH SFTP wildcard WINMISC

doc/config.but
@@ -2728,6 +2728,27 @@ connections fail.
 PuTTY's default is \cw{MIT-MAGIC-COOKIE-1}. If you change it, you
 should be sure you know what you're doing.
 
+\S{config-ssh-xauthority} X authority file for local display
+
+\cfg{winhelp-topic}{ssh.tunnels.xauthority}
+
+If you are using X11 forwarding, the local X server to which your
+forwarded connections are eventually directed may itself require
+authorisation.
+
+Some Windows X servers do not require this: they do authorisation by
+simpler means, such as accepting any connection from the local
+machine but not from anywhere else. However, if your X server does
+require authorisation, then PuTTY needs to know what authorisation
+is required.
+
+One way in which this data might be made available is for the X
+server to store it somewhere in a file which has the same format
+as the Unix \c{.Xauthority} file. If this is how your Windows X
+server works, then you can tell PuTTY where to find this file by
+configuring this option. By default, PuTTY will not attempt to find
+any authorisation for your local display.
+
 \H{config-ssh-portfwd} \I{port forwarding}The Tunnels panel
 
 \cfg{winhelp-topic}{ssh.tunnels.portfwd}

putty.h
@@ -572,6 +572,7 @@ struct config_tag {
     int x11_forward;
     char x11_display[128];
     int x11_auth;
+    Filename xauthfile;
     /* port forwarding */
     int lport_acceptall; /* accept conns from hosts other than localhost */
     int rport_acceptall; /* same for remote forwarded ports (SSH-2 only) */

settings.c
@@ -446,6 +446,7 @@ void save_open_settings(void *sesskey, Config *cfg)
     write_setting_i(sesskey, "X11Forward", cfg->x11_forward);
     write_setting_s(sesskey, "X11Display", cfg->x11_display);
     write_setting_i(sesskey, "X11AuthType", cfg->x11_auth);
+    write_setting_filename(sesskey, "X11AuthFile", cfg->xauthfile);
     write_setting_i(sesskey, "LocalPortAcceptAll", cfg->lport_acceptall);
     write_setting_i(sesskey, "RemotePortAcceptAll", cfg->rport_acceptall);
     wmap(sesskey, "PortForwardings", cfg->portfwd, lenof(cfg->portfwd));
@@ -775,6 +776,7 @@ void load_open_settings(void *sesskey, Config *cfg)
     gpps(sesskey, "X11Display", "", cfg->x11_display,
 	 sizeof(cfg->x11_display));
     gppi(sesskey, "X11AuthType", X11_MIT, &cfg->x11_auth);
+    gppfile(sesskey, "X11AuthFile", &cfg->xauthfile);
 
     gppi(sesskey, "LocalPortAcceptAll", 0, &cfg->lport_acceptall);
     gppi(sesskey, "RemotePortAcceptAll", 0, &cfg->rport_acceptall);

ssh.c
@@ -824,7 +824,7 @@ struct ssh_tag {
     Pkt_KCtx pkt_kctx;
     Pkt_ACtx pkt_actx;
 
-    void *x11auth;
+    struct X11Display *x11disp;
 
     int version;
     int conn_throttle_count;
@@ -4578,8 +4578,8 @@ static void ssh1_smsg_x11_open(Ssh ssh, struct Packet *pktin)
 	c = snew(struct ssh_channel);
 	c->ssh = ssh;
 
-	if (x11_init(&c->u.x11.s, ssh->cfg.x11_display, c,
-		     ssh->x11auth, NULL, -1, &ssh->cfg) != NULL) {
+	if (x11_init(&c->u.x11.s, ssh->x11disp, c,
+		     NULL, -1, &ssh->cfg) != NULL) {
 	    logevent("Opening X11 forward connection failed");
 	    sfree(c);
 	    send_packet(ssh, SSH1_MSG_CHANNEL_OPEN_FAILURE,
@@ -4914,11 +4914,9 @@ static void do_ssh1_connection(Ssh ssh, unsigned char *in, int inlen,
     }
 
     if (ssh->cfg.x11_forward) {
-	char proto[20], data[64];
 	logevent("Requesting X11 forwarding");
-	ssh->x11auth = x11_invent_auth(proto, sizeof(proto),
-				       data, sizeof(data), ssh->cfg.x11_auth);
-        x11_get_real_auth(ssh->x11auth, ssh->cfg.x11_display);
+	ssh->x11disp = x11_setup_display(ssh->cfg.x11_display,
+					 ssh->cfg.x11_auth, &ssh->cfg);
 	/*
 	 * Note that while we blank the X authentication data here, we don't
 	 * take any special action to blank the start of an X11 channel,
@@ -4928,14 +4926,19 @@ static void do_ssh1_connection(Ssh ssh, unsigned char *in, int inlen,
 	 */
 	if (ssh->v1_local_protoflags & SSH1_PROTOFLAG_SCREEN_NUMBER) {
 	    send_packet(ssh, SSH1_CMSG_X11_REQUEST_FORWARDING,
-			PKT_STR, proto,
-			PKTT_PASSWORD, PKT_STR, data, PKTT_OTHER,
-			PKT_INT, x11_get_screen_number(ssh->cfg.x11_display),
+			PKT_STR, ssh->x11disp->remoteauthprotoname,
+			PKTT_PASSWORD,
+			PKT_STR, ssh->x11disp->remoteauthdatastring,
+			PKTT_OTHER,
+			PKT_INT, ssh->x11disp->screennum,
 			PKT_END);
 	} else {
 	    send_packet(ssh, SSH1_CMSG_X11_REQUEST_FORWARDING,
-			PKT_STR, proto,
-			PKTT_PASSWORD, PKT_STR, data, PKTT_OTHER, PKT_END);
+			PKT_STR, ssh->x11disp->remoteauthprotoname,
+			PKTT_PASSWORD,
+			PKT_STR, ssh->x11disp->remoteauthdatastring,
+			PKTT_OTHER,
+			PKT_END);
 	}
 	do {
 	    crReturnV;
@@ -6939,9 +6942,8 @@ static void ssh2_msg_channel_open(Ssh ssh, struct Packet *pktin)
 
 	if (!ssh->X11_fwd_enabled)
 	    error = "X11 forwarding is not enabled";
-	else if (x11_init(&c->u.x11.s, ssh->cfg.x11_display, c,
-			  ssh->x11auth, addrstr, peerport,
-			  &ssh->cfg) != NULL) {
+	else if (x11_init(&c->u.x11.s, ssh->x11disp, c,
+			  addrstr, peerport, &ssh->cfg) != NULL) {
 	    error = "Unable to open an X11 connection";
 	} else {
 	    logevent("Opening X11 forward connection succeeded");
@@ -8479,17 +8481,15 @@ static void do_ssh2_authconn(Ssh ssh, unsigned char *in, int inlen,
      * Potentially enable X11 forwarding.
      */
     if (ssh->mainchan && !ssh->ncmode && ssh->cfg.x11_forward) {
-	char proto[20], data[64];
 	logevent("Requesting X11 forwarding");
-	ssh->x11auth = x11_invent_auth(proto, sizeof(proto),
-				       data, sizeof(data), ssh->cfg.x11_auth);
-        x11_get_real_auth(ssh->x11auth, ssh->cfg.x11_display);
+	ssh->x11disp = x11_setup_display(ssh->cfg.x11_display,
+					 ssh->cfg.x11_auth, &ssh->cfg);
 	s->pktout = ssh2_pkt_init(SSH2_MSG_CHANNEL_REQUEST);
 	ssh2_pkt_adduint32(s->pktout, ssh->mainchan->remoteid);
 	ssh2_pkt_addstring(s->pktout, "x11-req");
 	ssh2_pkt_addbool(s->pktout, 1);	       /* want reply */
 	ssh2_pkt_addbool(s->pktout, 0);	       /* many connections */
-	ssh2_pkt_addstring(s->pktout, proto);
+	ssh2_pkt_addstring(s->pktout, ssh->x11disp->remoteauthprotoname);
 	/*
 	 * Note that while we blank the X authentication data here, we don't
 	 * take any special action to blank the start of an X11 channel,
@@ -8498,9 +8498,9 @@ static void do_ssh2_authconn(Ssh ssh, unsigned char *in, int inlen,
 	 * cookie into the log.
 	 */
 	dont_log_password(ssh, s->pktout, PKTLOG_BLANK);
-	ssh2_pkt_addstring(s->pktout, data);
+	ssh2_pkt_addstring(s->pktout, ssh->x11disp->remoteauthdatastring);
 	end_log_omission(ssh, s->pktout);
-	ssh2_pkt_adduint32(s->pktout, x11_get_screen_number(ssh->cfg.x11_display));
+	ssh2_pkt_adduint32(s->pktout, ssh->x11disp->screennum);
 	ssh2_pkt_send(ssh, s->pktout);
 
 	crWaitUntilV(pktin);
@@ -8991,7 +8991,7 @@ static const char *ssh_init(void *frontend_handle, void **backend_handle,
     ssh->fallback_cmd = 0;
     ssh->pkt_kctx = SSH2_PKTCTX_NOKEX;
     ssh->pkt_actx = SSH2_PKTCTX_NOAUTH;
-    ssh->x11auth = NULL;
+    ssh->x11disp = NULL;
     ssh->v1_compressing = FALSE;
     ssh->v2_outgoing_sequence = 0;
     ssh->ssh1_rdpkt_crstate = 0;
@@ -9129,8 +9129,8 @@ static void ssh_free(void *handle)
 	ssh->rportfwds = NULL;
     }
     sfree(ssh->deferred_send_data);
-    if (ssh->x11auth)
-	x11_free_auth(ssh->x11auth);
+    if (ssh->x11disp)
+	x11_free_display(ssh->x11disp);
     sfree(ssh->do_ssh_init_state);
     sfree(ssh->do_ssh1_login_state);
     sfree(ssh->do_ssh2_transport_state);

ssh.h
@@ -2,6 +2,7 @@
 #include <string.h>
 
 #include "puttymem.h"
+#include "tree234.h"
 #include "network.h"
 #include "int64.h"
 #include "misc.h"
@@ -327,28 +328,85 @@ extern void pfd_unthrottle(Socket s);
 extern void pfd_override_throttle(Socket s, int enable);
 
 /* Exports from x11fwd.c */
-extern const char *x11_init(Socket *, char *, void *, void *, const char *,
-			    int, const Config *);
+enum {
+    X11_TRANS_IPV4 = 0, X11_TRANS_IPV6 = 6, X11_TRANS_UNIX = 256
+};
+struct X11Display {
+    /* Broken-down components of the display name itself */
+    int unixdomain;
+    char *hostname;
+    int displaynum;
+    int screennum;
+    /* OSX sometimes replaces all the above with a full Unix-socket pathname */
+    char *unixsocketpath;
+
+    /* PuTTY networking SockAddr to connect to the display, and associated
+     * gubbins */
+    SockAddr addr;
+    int port;
+    char *realhost;
+
+    /* Auth details we invented for the virtual display on the SSH server. */
+    int remoteauthproto;
+    unsigned char *remoteauthdata;
+    int remoteauthdatalen;
+    char *remoteauthprotoname;
+    char *remoteauthdatastring;
+
+    /* Our local auth details for talking to the real X display. */
+    int localauthproto;
+    unsigned char *localauthdata;
+    int localauthdatalen;
+
+    /*
+     * Used inside x11fwd.c to remember recently seen
+     * XDM-AUTHORIZATION-1 strings, to avoid replay attacks.
+     */
+    tree234 *xdmseen;
+};
+/*
+ * x11_setup_display() parses the display variable and fills in an
+ * X11Display structure. Some remote auth details are invented;
+ * the supplied authtype parameter configures the preferred
+ * authorisation protocol to use at the remote end. The local auth
+ * details are looked up by calling platform_get_x11_auth.
+ */
+extern struct X11Display *x11_setup_display(char *display, int authtype,
+					    const Config *);
+void x11_free_display(struct X11Display *disp);
+extern const char *x11_init(Socket *, struct X11Display *, void *,
+			    const char *, int, const Config *);
 extern void x11_close(Socket);
 extern int x11_send(Socket, char *, int);
-extern void *x11_invent_auth(char *, int, char *, int, int);
-extern void x11_free_auth(void *);
 extern void x11_unthrottle(Socket s);
 extern void x11_override_throttle(Socket s, int enable);
-extern int x11_get_screen_number(char *display);
-void x11_get_real_auth(void *authv, char *display);
 char *x11_display(const char *display);
-
 /* Platform-dependent X11 functions */
-extern void platform_get_x11_auth(char *display, int *proto,
-                                  unsigned char *data, int *datalen);
-extern const char platform_x11_best_transport[];
-/* best X11 hostname for this platform if none specified */
-SockAddr platform_get_x11_unix_address(const char *display, int displaynum,
-				       char **canonicalname);
-/* make up a SockAddr naming the address for displaynum */
+extern void platform_get_x11_auth(struct X11Display *display,
+				  const Config *);
+    /* examine a mostly-filled-in X11Display and fill in localauth* */
+extern const int platform_uses_x11_unix_by_default;
+    /* choose default X transport in the absence of a specified one */
+SockAddr platform_get_x11_unix_address(const char *path, int displaynum);
+    /* make up a SockAddr naming the address for displaynum */
 char *platform_get_x_display(void);
-/* allocated local X display string, if any */
+    /* allocated local X display string, if any */
+/* Callbacks in x11.c usable _by_ platform X11 functions */
+/*
+ * This function does the job of platform_get_x11_auth, provided
+ * it is told where to find a normally formatted .Xauthority file:
+ * it opens that file, parses it to find an auth record which
+ * matches the display details in "display", and fills in the
+ * localauth fields.
+ *
+ * It is expected that most implementations of
+ * platform_get_x11_auth() will work by finding their system's
+ * .Xauthority file, adjusting the display details if necessary
+ * for local oddities like Unix-domain socket transport, and
+ * calling this function to do the rest of the work.
+ */
+void x11_get_auth_from_authfile(struct X11Display *display,
+				const char *authfilename);
 
 Bignum copybn(Bignum b);
 Bignum bn_power_2(int n);

unix/ux_x11.c
@@ -5,117 +5,66 @@
 #include <ctype.h>
 #include <unistd.h>
 #include <assert.h>
+#include <stdlib.h>
+
 #include "putty.h"
 #include "ssh.h"
+#include "network.h"
 
-void platform_get_x11_auth(char *display, int *protocol,
-                           unsigned char *data, int *datalen)
+void platform_get_x11_auth(struct X11Display *disp, const Config *cfg)
 {
-    FILE *fp;
-    char *command;
-    int maxsize = *datalen;
-    char *localbuf;
-    int proto = -1;
+    char *xauthfile;
+    int needs_free;
 
-    display = x11_display(display);
     /*
-     * Normally we should run `xauth list DISPLAYNAME'. However,
-     * there's an oddity when the display is local: the display
-     * `localhost:0' (or `:0') should become just `:0'.
+     * Upgrade an IP-style localhost display to a Unix-socket
+     * display.
      */
-    if (!strncmp(display, "localhost:", 10)
-	|| !strncmp(display, "unix:", 5))
-	command = dupprintf("xauth list %s 2>/dev/null",
-			    strchr(display, ':'));
-    else
-	command = dupprintf("xauth list %s 2>/dev/null", display);
-    sfree(display);
-    fp = popen(command, "r");
-    sfree(command);
-
-    if (!fp)
-        return;                        /* assume no auth */
-
-    localbuf = snewn(maxsize, char);
-
-    while (1) {
-        /*
-         * Read a line from stdin, and attempt to parse it into a
-         * display name (ignored), auth protocol, and auth string.
-         */
-        int c, i, hexdigit;
-        char protoname[64];
-
-        /* Skip the display name. */
-        while (c = getc(fp), c != EOF && c != '\n' && !isspace(c));
-        if (c == EOF) break;
-        if (c == '\n') continue;
-
-        /* Skip white space. */
-        while (c != EOF && c != '\n' && isspace(c))
-            c = getc(fp);
-        if (c == EOF) break;
-        if (c == '\n') continue;
-
-        /* Read the auth protocol name, and see if it matches any we
-         * know about. */
-        i = 0;
-        while (c != EOF && c != '\n' && !isspace(c)) {
-            if (i < lenof(protoname)-1) protoname[i++] = c;
-            c = getc(fp);
-        }
-        protoname[i] = '\0';
-
-        for (i = X11_NO_AUTH; ++i < X11_NAUTHS ;) {
-            if (!strcmp(protoname, x11_authnames[i]))
-                break;
-        }
-        if (i >= X11_NAUTHS || i <= proto) {
-            /* Unrecognised protocol name, or a worse one than we already have.
-	     * Skip this line. */
-            while (c != EOF && c != '\n')
-                c = getc(fp);
-            if (c == EOF) break;
-        }
-        proto = i;
-
-        /* Skip white space. */
-        while (c != EOF && c != '\n' && isspace(c))
-            c = getc(fp);
-        if (c == EOF) break;
-        if (c == '\n') continue;
-
-        /*
-         * Now grab pairs of hex digits and shove them into `data'.
-         */
-        i = 0;
-        hexdigit = -1;
-        while (c != EOF && c != '\n') {
-            int hexval = -1;
-            if (c >= 'A' && c <= 'F')
-                hexval = c + 10 - 'A';
-            if (c >= 'a' && c <= 'f')
-                hexval = c + 10 - 'a';
-            if (c >= '0' && c <= '9')
-                hexval = c - '0';
-            if (hexval >= 0) {
-                if (hexdigit >= 0) {
-                    hexdigit = (hexdigit << 4) + hexval;
-                    if (i < maxsize)
-                        localbuf[i++] = hexdigit;
-                    hexdigit = -1;
-                } else
-                    hexdigit = hexval;
-            }
-            c = getc(fp);
-        }
-
-        *datalen = i;
-        *protocol = proto;
-	memcpy(data, localbuf, i);
-
-	/* Nonetheless, continue looping round; we might find a better one. */
+    if (!disp->unixdomain && sk_address_is_local(disp->addr)) {
+	sk_addr_free(disp->addr);
+	disp->unixdomain = TRUE;
+	disp->addr = platform_get_x11_unix_address(NULL, disp->displaynum);
+	disp->realhost = dupprintf("unix:%d", disp->displaynum);
+	disp->port = 0;
     }
-    pclose(fp);
-    sfree(localbuf);
+
+    /*
+     * Set the hostname for Unix-socket displays, so that we'll
+     * look it up correctly in the X authority file.
+     */
+    if (disp->unixdomain) {
+	int len;
+
+	sfree(disp->hostname);
+	len = 128;
+	do {
+	    len *= 2;
+	    disp->hostname = snewn(len, char);
+	    if (gethostname(disp->hostname, len) < 0) {
+		disp->hostname = NULL;
+		return;
+	    }
+	} while (strlen(disp->hostname) >= len-1);
+    }
+
+    /*
+     * Find the .Xauthority file.
+     */
+    needs_free = FALSE;
+    xauthfile = getenv("XAUTHORITY");
+    if (!xauthfile) {
+	xauthfile = getenv("HOME");
+	if (xauthfile) {
+	    xauthfile = dupcat(xauthfile, "/.Xauthority", NULL);
+	    needs_free = TRUE;
+	}
+    }
+
+    if (xauthfile) {
+	x11_get_auth_from_authfile(disp, xauthfile);
+	if (needs_free)
+	    sfree(xauthfile);
+    }
 }
+
+const int platform_uses_x11_unix_by_default = TRUE;

commit 01a7673ae9b20274184c3a7dac690055ba2cd24c

Fixed a PuTTYtel startup assertion failure introduced in r8305. While I'm here, a cosmetic PuTTYtel change: remove a reference to SSH from the "logical host name" label in PuTTYtel only.
[originally from svn r8331]
[r8305 == ca6fc3a4daf51166a15693feffc967bee9e3f59a]

config.c
@@ -1729,13 +1729,17 @@ void setup_config_box(struct controlbox *b, int midsession,
 			  NULL);
 #endif
 
-	    s = ctrl_getset(b, "Connection", "identity",
-			    "Logical name of remote host");
-	    ctrl_editbox(s, "Logical name of remote host (e.g. for SSH key lookup):",
-			 'm', 100,
-			 HELPCTX(connection_loghost),
-			 dlg_stdeditbox_handler, I(offsetof(Config,loghost)),
-			 I(sizeof(((Config *)0)->loghost)));
+	    {
+		char *label = backend_from_proto(PROT_SSH) ?
+		    "Logical name of remote host (e.g. for SSH key lookup):" :
+		    "Logical name of remote host:";
+		s = ctrl_getset(b, "Connection", "identity",
+				"Logical name of remote host");
+		ctrl_editbox(s, label, 'm', 100,
+			     HELPCTX(connection_loghost),
+			     dlg_stdeditbox_handler, I(offsetof(Config,loghost)),
+			     I(sizeof(((Config *)0)->loghost)));
+	    }
 	}
 
 	/*

windows/wincfg.c
@@ -382,9 +382,11 @@ void win_setup_config_box(struct controlbox *b, HWND *hwndp, int has_help,
      * $XAUTHORITY is not reliable on Windows, so we provide a
      * means to override it.
      */
-    s = ctrl_getset(b, "Connection/SSH/X11", "x11", "X11 forwarding");
-    ctrl_filesel(s, "X authority file for local display", 't',
-		 NULL, FALSE, "Select X authority file",
-		 HELPCTX(ssh_tunnels_xauthority),
-		 dlg_stdfilesel_handler, I(offsetof(Config, xauthfile)));
+    if (backend_from_proto(PROT_SSH)) {
+	s = ctrl_getset(b, "Connection/SSH/X11", "x11", "X11 forwarding");
+	ctrl_filesel(s, "X authority file for local display", 't',
+		     NULL, FALSE, "Select X authority file",
+		     HELPCTX(ssh_tunnels_xauthority),
+		     dlg_stdfilesel_handler, I(offsetof(Config, xauthfile)));
+    }
 }