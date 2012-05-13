0395e52bb8db4b77e792ea1c46b2c0024c67986b
Simon Tatham <anakin@pobox.com>
2012-05-13 15:59:27 +0000

    Use clock_gettime(CLOCK_MONOTONIC) as the Unix getticks(), if it's available.
    [originally from svn r9529]

--- unix/uxmisc.c
+++ unix/uxmisc.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <unistd.h>
+#include <time.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <pwd.h>
@@ -15,14 +16,27 @@
 
 unsigned long getticks(void)
 {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
     /*
-     * We want to use milliseconds rather than microseconds,
-     * because we need a decent number of them to fit into a 32-bit
-     * word so it can be used for keepalives.
+     * We want to use milliseconds rather than the microseconds or
+     * nanoseconds given by the underlying clock functions, because we
+     * need a decent number of them to fit into a 32-bit word so it
+     * can be used for keepalives.
      */
-    return tv.tv_sec * TICKSPERSEC + tv.tv_usec / (1000000 / TICKSPERSEC);
+#if defined HAVE_CLOCK_GETTIME && defined HAVE_DECL_CLOCK_MONOTONIC
+    {
+        /* Use CLOCK_MONOTONIC if available, so as to be unconfused if
+         * the system clock changes. */
+        struct timespec ts;
+        if (clock_gettime(CLOCK_MONOTONIC, &ts) == 0)
+            return ts.tv_sec * TICKSPERSEC +
+                ts.tv_nsec / (1000000 / TICKSPERSEC);
+    }
+#endif
+    {
+        struct timeval tv;
+        gettimeofday(&tv, NULL);
+        return tv.tv_sec * TICKSPERSEC + tv.tv_usec / (1000000 / TICKSPERSEC);
+    }
 }
 
 Filename *filename_from_str(const char *str)
