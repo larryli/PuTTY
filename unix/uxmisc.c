35fde00fd1fdc084a78dc3e4c3f94dbf16bbd236
Simon Tatham <anakin@pobox.com>
2015-05-18 21:17:21 +0100

    Fix a compile warning with -DDEBUG.
    An unguarded write() in the dputs function caused gcc -Werror to fail
    to compile. I'm confused that this hasn't bitten me before, though -
    obviously normal builds of PuTTY condition out the faulty code, but
    _surely_ this can't be the first time I've enabled the developer
    diagnostics since gcc started complaining about unchecked syscall
    returns!

--- unix/uxmisc.c
+++ unix/uxmisc.c
@@ -104,7 +104,7 @@ void dputs(const char *buf)
 	debug_fp = fopen("debug.log", "w");
     }
 
-    write(1, buf, strlen(buf));
+    if (write(1, buf, strlen(buf)) < 0) {} /* 'error check' to placate gcc */
 
     fputs(buf, debug_fp);
     fflush(debug_fp);
