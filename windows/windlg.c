4327fe71fe599e7a34d72541b792f5fcbb5614df
Simon Tatham <anakin@pobox.com>
2016-02-25 20:42:00 +0000

    Use readonly edit controls in some Windows dialogs.
    This makes the About and Licence boxes copy-and-pasteable, similarly
    to what I've just done on Unix.
    
    (But unlike on the Unix side, here I haven't touched the host key
    prompt dialog, because that's a standard Windows MessageBox and not
    easy to mess around with. Plus, in any case, you can already hit ^C to
    copy the whole text out of a MessageBox. Same goes for the PGP
    fingerprints dialog.)
    
    As a side effect, several copies of the copyright notice and licence
    text have moved from .rc files into C source. I've updated
    CHECKLST.txt, but they won't stay there for long.
    
    (cherry picked from commit 2eb952ca31aa13d1f6f429305fbb6f43a9a28c56)
    
    Conflicts:
    	windows/pageant.rc
    	windows/puttygen.rc
    	windows/win_res.rc2
    
    (cherry-picker's notes: the conflict was just because several copies
    of the licence text were deleted, and they weren't quite the same
    between branches)

--- windows/windlg.c
+++ windows/windlg.c
@@ -170,6 +170,37 @@ static int CALLBACK LicenceProc(HWND hwnd, UINT msg,
 	    char *str = dupprintf("%s Licence", appname);
 	    SetWindowText(hwnd, str);
 	    sfree(str);
+
+            SetDlgItemText(hwnd, IDA_TEXT,
+	"Copyright 1997-2015 Simon Tatham.\r\n\r\n"
+
+	"Portions copyright Robert de Bath, Joris van Rantwijk, Delian "
+	"Delchev, Andreas Schultz, Jeroen Massar, Wez Furlong, Nicolas "
+	"Barry, Justin Bradford, Ben Harris, Malcolm Smith, Ahmad Khalifa, "
+	"Markus Kuhn, Colin Watson, Christopher Staite, and CORE SDI S.A.\r\n\r\n"
+
+	"Permission is hereby granted, free of charge, to any person "
+	"obtaining a copy of this software and associated documentation "
+	"files (the ""Software""), to deal in the Software without restriction, "
+	"including without limitation the rights to use, copy, modify, merge, "
+	"publish, distribute, sublicense, and/or sell copies of the Software, "
+	"and to permit persons to whom the Software is furnished to do so, "
+	"subject to the following conditions:\r\n\r\n"
+
+	"The above copyright notice and this permission notice shall be "
+	"included in all copies or substantial portions of the Software.\r\n\r\n"
+
+	"THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT "
+	"WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, "
+	"INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF "
+	"MERCHANTABILITY, FITNESS FOR A PARTICULAR "
+	"PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE "
+	"COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES "
+	"OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, "
+	"TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN "
+	"CONNECTION WITH THE SOFTWARE OR THE USE OR "
+	"OTHER DEALINGS IN THE SOFTWARE."
+);
 	}
 	return 1;
       case WM_COMMAND:
@@ -197,8 +228,14 @@ static int CALLBACK AboutProc(HWND hwnd, UINT msg,
 	str = dupprintf("About %s", appname);
 	SetWindowText(hwnd, str);
 	sfree(str);
-	SetDlgItemText(hwnd, IDA_TEXT1, appname);
-	SetDlgItemText(hwnd, IDA_VERSION, ver);
+        {
+            char *text = dupprintf
+                ("%s\r\n\r\n%s\r\n\r\n%s",
+                 appname, ver,
+                 "\251 1997-2015 Simon Tatham. All rights reserved.");
+            SetDlgItemText(hwnd, IDA_TEXT, text);
+            sfree(text);
+        }
 	return 1;
       case WM_COMMAND:
 	switch (LOWORD(wParam)) {
