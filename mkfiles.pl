68b19333268880f7c70207b1976d6700d281e3f1
Simon Tatham <anakin@pobox.com>
2016-03-25 09:10:50 +0000

    Fix Makefile.gtk in the wake of the new [XT] program type.
    It's not the recommended makefile any more, but it's not too hard
    to keep it working for the moment.

--- mkfiles.pl
+++ mkfiles.pl
@@ -1398,11 +1398,12 @@ if (defined $makefiles{'gtk'}) {
     print "\n\n";
     foreach $p (&prognames("X:XT:U:UT")) {
       ($prog, $type) = split ",", $p;
+      ($ldflags = $type) =~ s/T$//;
       $objstr = &objects($p, "X.o", undef, undef);
       print &splitline($prog . ": " . $objstr), "\n";
       $libstr = &objects($p, undef, undef, "-lX");
       print &splitline("\t\$(CC) -o \$@ " .
-                       $objstr . " \$(${type}LDFLAGS) $libstr", 69), "\n\n";
+                       $objstr . " \$(${ldflags}LDFLAGS) $libstr", 69), "\n\n";
     }
     foreach $d (&deps("X.o", undef, $dirpfx, "/", "gtk")) {
       if ($forceobj{$d->{obj_orig}}) {
