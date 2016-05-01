05e802d5fe8d34b326ad251d5e25b76f7d006b33
Ben Harris <bjh21@bjh21.me.uk>
2016-05-01 18:11:50 +0200

    uxcons: Don't try to include <sys/select.h> on systems that don't have it.
    Also include <sys/time.h> for ancient systems that define select()
    there.

--- unix/uxcons.c
+++ unix/uxcons.c
@@ -12,7 +12,10 @@
 #include <termios.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/time.h>
+#ifndef HAVE_NO_SYS_SELECT_H
 #include <sys/select.h>
+#endif
 
 #include "putty.h"
 #include "storage.h"
