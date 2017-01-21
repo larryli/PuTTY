d3c52715d8a6156a9ea22139602838840fb9daf3
Simon Tatham <anakin@pobox.com>
2017-01-21 15:17:03 +0000

    Rename the maps directory in release.pl.
    The code that copies the link maps of the release Windows builds into
    the place I store them for later debugging should now not
    embarrassingly look in the wrong place when we make our first
    post-VS2015 release.

--- release.pl
+++ release.pl
@@ -68,10 +68,10 @@ if ($upload) {
     defined $version or die "use --version";
 
     # Run this inside the build.out directory.
-    -d "maps-x86" or die "no maps-x86 directory in cwd";
+    -d "maps" or die "no maps directory in cwd";
     -d "putty" or die "no putty directory in cwd";
 
-    0 == system("rsync", "-av", "maps-x86/",
+    0 == system("rsync", "-av", "maps/",
                 "atreus:src/putty-local/maps-$version")
         or die "could not upload link maps";
 
