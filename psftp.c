988e26068eead42aed72573cc1cab14cb9bfebe7
Owen Dunn <owen@greenend.org.uk>
2017-02-18 22:46:38 +0000

    Regard dir/ls on non-existent directory as an error.
    sftp_cmd_ls - return an error if attempting to open the directory
    fails.

--- psftp.c
+++ psftp.c
@@ -1050,6 +1050,7 @@ int sftp_cmd_ls(struct sftp_command *cmd)
 
     if (dirh == NULL) {
 	printf("Unable to open %s: %s\n", dir, fxp_error());
+	return 0;
     } else {
 	nnames = namesize = 0;
 	ournames = NULL;
