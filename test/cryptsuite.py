larryli/PuTTY

commit 4988fd410cf62aa1e7d8c8f34013bcd690b86a7b

Replace all uses of SHA*_Bytes / MD5Update.
In fact, those functions don't even exist any more. The only way to
get data into a primitive hash state is via the new put_* system. Of
course, that means put_data() is a viable replacement for every
previous call to one of the per-hash update functions - but just
mechanically doing that would have missed the opportunity to simplify
a lot of the call sites.

sshrsa.c
@@ -219,7 +219,7 @@ static Bignum rsa_privkey_op(Bignum input, struct RSAKey *key)
 		 */
 		if (digestused >= lenof(digest512)) {
 		    SHA512_Init(&ss);
-		    SHA512_Bytes(&ss, "RSA deterministic blinding", 26);
+		    put_data(&ss, "RSA deterministic blinding", 26);
 		    put_uint32(&ss, hashseq);
 		    put_mp_ssh2(&ss, key->private_exponent);
 		    SHA512_Final(&ss, digest512);
@@ -230,7 +230,7 @@ static Bignum rsa_privkey_op(Bignum input, struct RSAKey *key)
 		     * input.
 		     */
 		    SHA512_Init(&ss);
-		    SHA512_Bytes(&ss, digest512, sizeof(digest512));
+		    put_data(&ss, digest512, sizeof(digest512));
 		    put_mp_ssh2(&ss, input);
 		    SHA512_Final(&ss, digest512);
 
@@ -351,19 +351,11 @@ void rsa_fingerprint(char *str, int len, struct RSAKey *key)
     struct MD5Context md5c;
     unsigned char digest[16];
     char buffer[16 * 3 + 40];
-    int numlen, slen, i;
+    int slen, i;
 
     MD5Init(&md5c);
-    numlen = ssh1_bignum_length(key->modulus) - 2;
-    for (i = numlen; i--;) {
-	unsigned char c = bignum_byte(key->modulus, i);
-	MD5Update(&md5c, &c, 1);
-    }
-    numlen = ssh1_bignum_length(key->exponent) - 2;
-    for (i = numlen; i--;) {
-	unsigned char c = bignum_byte(key->exponent, i);
-	MD5Update(&md5c, &c, 1);
-    }
+    put_mp_ssh1(&md5c, key->modulus);
+    put_mp_ssh1(&md5c, key->exponent);
     MD5Final(digest, &md5c);
 
     sprintf(buffer, "%d ", bignum_bitcount(key->modulus));

import.c
@@ -602,14 +602,14 @@ struct ssh2_userkey *openssh_pem_read(const Filename *filename,
         unsigned char keybuf[32];
 
         MD5Init(&md5c);
-        MD5Update(&md5c, (unsigned char *)passphrase, strlen(passphrase));
-        MD5Update(&md5c, (unsigned char *)key->iv, 8);
+        put_data(&md5c, passphrase, strlen(passphrase));
+        put_data(&md5c, key->iv, 8);
         MD5Final(keybuf, &md5c);
 
         MD5Init(&md5c);
-        MD5Update(&md5c, keybuf, 16);
-        MD5Update(&md5c, (unsigned char *)passphrase, strlen(passphrase));
-        MD5Update(&md5c, (unsigned char *)key->iv, 8);
+        put_data(&md5c, keybuf, 16);
+        put_data(&md5c, passphrase, strlen(passphrase));
+        put_data(&md5c, key->iv, 8);
         MD5Final(keybuf+16, &md5c);
 
         /*
@@ -1201,14 +1201,14 @@ int openssh_pem_write(const Filename *filename, struct ssh2_userkey *key,
 	for (i = 0; i < 8; i++) iv[i] = random_byte();
 
 	MD5Init(&md5c);
-	MD5Update(&md5c, (unsigned char *)passphrase, strlen(passphrase));
-	MD5Update(&md5c, iv, 8);
+	put_data(&md5c, passphrase, strlen(passphrase));
+	put_data(&md5c, iv, 8);
 	MD5Final(keybuf, &md5c);
 
 	MD5Init(&md5c);
-	MD5Update(&md5c, keybuf, 16);
-	MD5Update(&md5c, (unsigned char *)passphrase, strlen(passphrase));
-	MD5Update(&md5c, iv, 8);
+	put_data(&md5c, keybuf, 16);
+	put_data(&md5c, passphrase, strlen(passphrase));
+	put_data(&md5c, iv, 8);
 	MD5Final(keybuf+16, &md5c);
 
 	/*
@@ -2362,12 +2362,12 @@ struct ssh2_userkey *sshcom_read(const Filename *filename, char *passphrase,
         }
 
 	MD5Init(&md5c);
-	MD5Update(&md5c, (unsigned char *)passphrase, strlen(passphrase));
+	put_data(&md5c, passphrase, strlen(passphrase));
 	MD5Final(keybuf, &md5c);
 
 	MD5Init(&md5c);
-	MD5Update(&md5c, (unsigned char *)passphrase, strlen(passphrase));
-	MD5Update(&md5c, keybuf, 16);
+	put_data(&md5c, passphrase, strlen(passphrase));
+	put_data(&md5c, keybuf, 16);
 	MD5Final(keybuf+16, &md5c);
 
 	/*
@@ -2646,12 +2646,12 @@ int sshcom_write(const Filename *filename, struct ssh2_userkey *key,
 	unsigned char keybuf[32], iv[8];
 
 	MD5Init(&md5c);
-	MD5Update(&md5c, (unsigned char *)passphrase, strlen(passphrase));
+	put_data(&md5c, passphrase, strlen(passphrase));
 	MD5Final(keybuf, &md5c);
 
 	MD5Init(&md5c);
-	MD5Update(&md5c, (unsigned char *)passphrase, strlen(passphrase));
-	MD5Update(&md5c, keybuf, 16);
+	put_data(&md5c, passphrase, strlen(passphrase));
+	put_data(&md5c, keybuf, 16);
 	MD5Final(keybuf+16, &md5c);
 
 	/*

pageant.c
@@ -452,7 +452,7 @@ void *pageant_handle_msg(const void *msg, int msglen, int *outlen,
 		response_source[i] = bignum_byte(response, 31 - i);
 
 	    MD5Init(&md5c);
-	    MD5Update(&md5c, response_source, 48);
+	    put_data(&md5c, response_source, 48);
 	    MD5Final(response_md5, &md5c);
 	    smemclr(response_source, 48);	/* burn the evidence */
 	    freebn(response);	       /* and that evidence */

ssh.c
@@ -4349,9 +4349,9 @@ static void do_ssh1_login(void *vctx)
     ssh->v1_local_protoflags |= SSH1_PROTOFLAG_SCREEN_NUMBER;
 
     MD5Init(&md5c);
-    MD5Update(&md5c, s->keystr2, s->hostkey.bytes);
-    MD5Update(&md5c, s->keystr1, s->servkey.bytes);
-    MD5Update(&md5c, s->cookie, 8);
+    put_data(&md5c, s->keystr2, s->hostkey.bytes);
+    put_data(&md5c, s->keystr1, s->servkey.bytes);
+    put_data(&md5c, s->cookie, 8);
     MD5Final(s->session_id, &md5c);
 
     for (i = 0; i < 32; i++)
@@ -4950,8 +4950,8 @@ static void do_ssh1_login(void *vctx)
 		    }
 
 		    MD5Init(&md5c);
-		    MD5Update(&md5c, buffer, 32);
-		    MD5Update(&md5c, s->session_id, 16);
+		    put_data(&md5c, buffer, 32);
+		    put_data(&md5c, s->session_id, 16);
 		    MD5Final(buffer, &md5c);
 
 		    send_packet(ssh, SSH1_CMSG_AUTH_RSA_RESPONSE,

ssh.h
@@ -254,8 +254,6 @@ struct MD5Context {
 };
 
 void MD5Init(struct MD5Context *context);
-void MD5Update(struct MD5Context *context, unsigned char const *buf,
-	       unsigned len);
 void MD5Final(unsigned char digest[16], struct MD5Context *context);
 void MD5Simple(void const *p, unsigned len, unsigned char output[16]);
 
@@ -276,7 +274,6 @@ typedef struct SHA_State {
     BinarySink_IMPLEMENTATION;
 } SHA_State;
 void SHA_Init(SHA_State * s);
-void SHA_Bytes(SHA_State * s, const void *p, int len);
 void SHA_Final(SHA_State * s, unsigned char *output);
 void SHA_Simple(const void *p, int len, unsigned char *output);
 
@@ -291,7 +288,6 @@ typedef struct SHA256_State {
     BinarySink_IMPLEMENTATION;
 } SHA256_State;
 void SHA256_Init(SHA256_State * s);
-void SHA256_Bytes(SHA256_State * s, const void *p, int len);
 void SHA256_Final(SHA256_State * s, unsigned char *output);
 void SHA256_Simple(const void *p, int len, unsigned char *output);
 
@@ -304,11 +300,9 @@ typedef struct {
 } SHA512_State;
 #define SHA384_State SHA512_State
 void SHA512_Init(SHA512_State * s);
-void SHA512_Bytes(SHA512_State * s, const void *p, int len);
 void SHA512_Final(SHA512_State * s, unsigned char *output);
 void SHA512_Simple(const void *p, int len, unsigned char *output);
 void SHA384_Init(SHA384_State * s);
-#define SHA384_Bytes(s, p, len) SHA512_Bytes(s, p, len)
 void SHA384_Final(SHA384_State * s, unsigned char *output);
 void SHA384_Simple(const void *p, int len, unsigned char *output);
 

sshbcrypt.c
@@ -56,16 +56,13 @@ void bcrypt_genblock(int counter,
 {
     SHA512_State shastate;
     unsigned char hashed_salt[64];
-    unsigned char countbuf[4];
 
     /* Hash the input salt with the counter value optionally suffixed
      * to get our real 32-byte salt */
     SHA512_Init(&shastate);
-    SHA512_Bytes(&shastate, salt, saltbytes);
-    if (counter) {
-        PUT_32BIT_MSB_FIRST(countbuf, counter);
-        SHA512_Bytes(&shastate, countbuf, 4);
-    }
+    put_data(&shastate, salt, saltbytes);
+    if (counter)
+        put_uint32(&shastate, counter);
     SHA512_Final(&shastate, hashed_salt);
 
     bcrypt_hash(hashed_passphrase, 64, hashed_salt, 64, output);

sshdss.c
@@ -540,7 +540,7 @@ Bignum *dss_gen_k(const char *id_string, Bignum modulus, Bignum private_key,
      * Hash some identifying text plus x.
      */
     SHA512_Init(&ss);
-    SHA512_Bytes(&ss, id_string, strlen(id_string) + 1);
+    put_asciz(&ss, id_string);
     put_mp_ssh2(&ss, private_key);
     SHA512_Final(&ss, digest512);
 
@@ -548,8 +548,8 @@ Bignum *dss_gen_k(const char *id_string, Bignum modulus, Bignum private_key,
      * Now hash that digest plus the message hash.
      */
     SHA512_Init(&ss);
-    SHA512_Bytes(&ss, digest512, sizeof(digest512));
-    SHA512_Bytes(&ss, digest, digest_len);
+    put_data(&ss, digest512, sizeof(digest512));
+    put_data(&ss, digest, digest_len);
 
     while (1) {
         SHA512_State ss2 = ss;         /* structure copy */
@@ -573,7 +573,7 @@ Bignum *dss_gen_k(const char *id_string, Bignum modulus, Bignum private_key,
         /* Very unlikely we get here, but if so, k was unsuitable. */
         freebn(k);
         /* Perturb the hash to think of a different k. */
-        SHA512_Bytes(&ss, "x", 1);
+        put_byte(&ss, 'x');
         /* Go round and try again. */
     }
 }

sshecc.c
@@ -1421,10 +1421,8 @@ struct ec_point *ec_public(const Bignum privateKey, const struct ec_curve *curve
         SHA512_Init(&s);
 
         keylen = curve->fieldBits / 8;
-        for (i = 0; i < keylen; ++i) {
-            unsigned char b = bignum_byte(privateKey, i);
-            SHA512_Bytes(&s, &b, 1);
-        }
+        for (i = 0; i < keylen; ++i)
+            put_byte(&s, bignum_byte(privateKey, i));
         SHA512_Final(&s, hash);
 
         /* The second part is simply turning the hash into a Bignum,
@@ -2298,27 +2296,25 @@ static int ecdsa_verifysig(void *key, const char *sig, int siglen,
         /* Get the hash of the encoded value of R + encoded value of pk + message */
         {
             int i, pointlen;
-            unsigned char b;
             unsigned char digest[512 / 8];
             SHA512_State hs;
             SHA512_Init(&hs);
 
-            /* Add encoded r (no need to encode it again, it was in the signature) */
-            SHA512_Bytes(&hs, p, ec->publicKey.curve->fieldBits / 8);
+            pointlen = ec->publicKey.curve->fieldBits / 8;
+
+            /* Add encoded r (no need to encode it again, it was in
+             * the signature) */
+            put_data(&hs, p, pointlen);
 
             /* Encode pk and add it */
-            pointlen = ec->publicKey.curve->fieldBits / 8;
-            for (i = 0; i < pointlen - 1; ++i) {
-                b = bignum_byte(ec->publicKey.y, i);
-                SHA512_Bytes(&hs, &b, 1);
-            }
+            for (i = 0; i < pointlen - 1; ++i)
+                put_byte(&hs, bignum_byte(ec->publicKey.y, i));
             /* Unset last bit of y and set first bit of x in its place */
-            b = bignum_byte(ec->publicKey.y, i) & 0x7f;
-            b |= bignum_bit(ec->publicKey.x, 0) << 7;
-            SHA512_Bytes(&hs, &b, 1);
+            put_byte(&hs, ((bignum_byte(ec->publicKey.y, i) & 0x7f) |
+                           (bignum_bit(ec->publicKey.x, 0) << 7)));
 
             /* Add the message itself */
-            SHA512_Bytes(&hs, data, datalen);
+            put_data(&hs, data, datalen);
 
             /* Get the hash */
             SHA512_Final(&hs, digest);
@@ -2425,15 +2421,12 @@ static unsigned char *ecdsa_sign(void *key, const char *data, int datalen,
          * S = (r + H(encodepoint(R) + encodepoint(pk) + m) * a) % l */
         {
             unsigned char hash[512/8];
-            unsigned char b;
             Bignum a;
             SHA512_State hs;
             SHA512_Init(&hs);
 
-            for (i = 0; i < pointlen; ++i) {
-                unsigned char b = (unsigned char)bignum_byte(ec->privateKey, i);
-                SHA512_Bytes(&hs, &b, 1);
-            }
+            for (i = 0; i < pointlen; ++i)
+                put_byte(&hs, bignum_byte(ec->privateKey, i));
 
             SHA512_Final(&hs, hash);
 
@@ -2447,11 +2440,10 @@ static unsigned char *ecdsa_sign(void *key, const char *data, int datalen,
             a = bignum_from_bytes_le(hash, 32);
 
             SHA512_Init(&hs);
-            SHA512_Bytes(&hs,
-                         hash+(ec->publicKey.curve->fieldBits / 8),
-                         (ec->publicKey.curve->fieldBits / 4)
-                         - (ec->publicKey.curve->fieldBits / 8));
-            SHA512_Bytes(&hs, data, datalen);
+            put_data(&hs, hash+(ec->publicKey.curve->fieldBits / 8),
+                     ((ec->publicKey.curve->fieldBits / 4) -
+                      (ec->publicKey.curve->fieldBits / 8)));
+            put_data(&hs, data, datalen);
             SHA512_Final(&hs, hash);
 
             r = bignum_from_bytes_le(hash, 512/8);
@@ -2465,27 +2457,21 @@ static unsigned char *ecdsa_sign(void *key, const char *data, int datalen,
             /* Now calculate s */
             SHA512_Init(&hs);
             /* Encode the point R */
-            for (i = 0; i < pointlen - 1; ++i) {
-                b = bignum_byte(rp->y, i);
-                SHA512_Bytes(&hs, &b, 1);
-            }
+            for (i = 0; i < pointlen - 1; ++i)
+                put_byte(&hs, bignum_byte(rp->y, i));
             /* Unset last bit of y and set first bit of x in its place */
-            b = bignum_byte(rp->y, i) & 0x7f;
-            b |= bignum_bit(rp->x, 0) << 7;
-            SHA512_Bytes(&hs, &b, 1);
+            put_byte(&hs, ((bignum_byte(rp->y, i) & 0x7f) |
+                           (bignum_bit(rp->x, 0) << 7)));
 
             /* Encode the point pk */
-            for (i = 0; i < pointlen - 1; ++i) {
-                b = bignum_byte(ec->publicKey.y, i);
-                SHA512_Bytes(&hs, &b, 1);
-            }
+            for (i = 0; i < pointlen - 1; ++i)
+                put_byte(&hs, bignum_byte(ec->publicKey.y, i));
             /* Unset last bit of y and set first bit of x in its place */
-            b = bignum_byte(ec->publicKey.y, i) & 0x7f;
-            b |= bignum_bit(ec->publicKey.x, 0) << 7;
-            SHA512_Bytes(&hs, &b, 1);
+            put_byte(&hs, ((bignum_byte(ec->publicKey.y, i) & 0x7f) |
+                           (bignum_bit(ec->publicKey.x, 0) << 7)));
 
             /* Add the message */
-            SHA512_Bytes(&hs, data, datalen);
+            put_data(&hs, data, datalen);
             SHA512_Final(&hs, hash);
 
             {

commit 4a0fa90979d43a6ba93ff8fa6a5ac81cf981e8d0

Fix wrong output from ssh1_rsa_fingerprint.
I broke it last year in commit 4988fd410, when I made hash contexts
expose a BinarySink interface. I went round finding no end of long-
winded ways of pushing things into hash contexts, often reimplementing
some standard thing like the wire formatting of an mpint, and rewrote
them more concisely using one or two put_foo calls.

But I failed to notice that the hash preimage used in SSH-1 key
fingerprints is _not_ implementable by put_ssh1_mpint! It consists of
the two public-key integers encoded in multi-byte binary big-endian
form, but without any preceding length field at all. I must have
looked too hastily, 'recognised' it as just implementing an mpint
formatter yet again, and replaced it with put_ssh1_mpint. So SSH-1 key
fingerprints have been completely wrong in the snapshots for months.

Fixed now, and this time, added a comment to warn me in case I get the
urge to simplify the code again, and a regression test in cryptsuite.

sshrsa.c
@@ -237,9 +237,19 @@ char *rsa_ssh1_fingerprint(RSAKey *key)
     strbuf *out;
     int i;
 
+    /*
+     * The hash preimage for SSH-1 key fingerprinting consists of the
+     * modulus and exponent _without_ any preceding length field -
+     * just the minimum number of bytes to represent each integer,
+     * stored big-endian, concatenated with no marker at the division
+     * between them.
+     */
+
     MD5Init(&md5c);
-    put_mp_ssh1(&md5c, key->modulus);
-    put_mp_ssh1(&md5c, key->exponent);
+    for (size_t i = (mp_get_nbits(key->modulus) + 7) / 8; i-- > 0 ;)
+        put_byte(&md5c, mp_get_byte(key->modulus, i));
+    for (size_t i = (mp_get_nbits(key->exponent) + 7) / 8; i-- > 0 ;)
+        put_byte(&md5c, mp_get_byte(key->exponent, i));
     MD5Final(digest, &md5c);
 
     out = strbuf_new();

test/cryptsuite.py
@@ -40,6 +40,12 @@ def ssh2_mpint(x):
     bytevals = [0xFF & (x >> (8*n)) for n in range(nbits(x)//8, -1, -1)]
     return struct.pack(">L" + "B" * len(bytevals), len(bytevals), *bytevals)
 
+def rsa_bare(e, n):
+    rsa = rsa_new()
+    get_rsa_ssh1_pub(ssh_uint32(nbits(n)) + ssh1_mpint(e) + ssh1_mpint(n),
+                     rsa, 'exponent_first')
+    return rsa
+
 def find_non_square_mod(p):
     # Find a non-square mod p, using the Jacobi symbol
     # calculation function from eccref.py.
@@ -736,6 +742,15 @@ class ecc(unittest.TestCase):
             rGi = ed25519.G * i
             self.assertEqual(int(x), int(rGi.x))
             self.assertEqual(int(y), int(rGi.y))
+
+class crypt(unittest.TestCase):
+    def testSSH1Fingerprint(self):
+        # Example key and reference fingerprint value generated by
+        # OpenSSH 6.7 ssh-keygen
+        rsa = rsa_bare(65537, 984185866443261798625575612408956568591522723900235822424492423996716524817102482330189709310179009158443944785704183009867662230534501187034891091310377917105259938712348098594526746211645472854839799025154390701673823298369051411)
+        fp = rsa_ssh1_fingerprint(rsa)
+        self.assertEqual(
+            fp, b"768 96:12:c8:bc:e6:03:75:86:e8:c7:b9:af:d8:0c:15:75")
 
 class standard_test_vectors(unittest.TestCase):
     def testAES(self):