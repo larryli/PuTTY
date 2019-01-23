621f8f4314d2976687f091419c85e70922adaf8e
Simon Tatham <anakin@pobox.com>
2019-01-23 23:29:57 +0000

    Windows: move dputs back into winmisc.c.
    Having it in winmiscs.c made it conflict with the one in testcrypt.

--- windows/winmiscs.c
+++ windows/winmiscs.c
@@ -16,33 +16,6 @@ void smemclr(void *b, size_t n) {
 }
 #endif
 
-#ifdef DEBUG
-static FILE *debug_fp = NULL;
-static HANDLE debug_hdl = INVALID_HANDLE_VALUE;
-static int debug_got_console = 0;
-
-void dputs(const char *buf)
-{
-    DWORD dw;
-
-    if (!debug_got_console) {
-	if (AllocConsole()) {
-	    debug_got_console = 1;
-	    debug_hdl = GetStdHandle(STD_OUTPUT_HANDLE);
-	}
-    }
-    if (!debug_fp) {
-	debug_fp = fopen("debug.log", "w");
-    }
-
-    if (debug_hdl != INVALID_HANDLE_VALUE) {
-	WriteFile(debug_hdl, buf, strlen(buf), &dw, NULL);
-    }
-    fputs(buf, debug_fp);
-    fflush(debug_fp);
-}
-#endif
-
 #ifdef MINEFIELD
 /*
  * Minefield - a Windows equivalent for Electric Fence
