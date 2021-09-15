larryli/PuTTY

commit 8f5e9a4f8dc796aee8789847c2a946d1065a7f39

Send PLUGLOG_CONNECT_SUCCESS in proxied socket types.
Now the non-SSH backends critically depend on it, it's important not
to forget to send it, for any socket type that's going to be used for
any of those backends. But ProxySocket, and the Unix and Windows
'socket' types wrapping pipes to local subprocesses, were not doing
so.

Some of these socket types don't have a SockAddr available to
represent the destination host. (Sometimes the concept isn't even
meaningful). Therefore, I've also expanded the semantics of
PLUGLOG_CONNECT_SUCCESS so that the addr parameter is allowed to be
NULL, and invented a noncommittal fallback version of the log message
in that situation.

network.h
@@ -67,8 +67,11 @@ struct PlugVtable {
      *    addresses to fall back to. When it _is_ fatal, the closing()
      *    function will be called.
      *
-     *  - PLUGLOG_CONNECT_SUCCESS means we have succeeded in
-     *    connecting to address `addr'.
+     *  - PLUGLOG_CONNECT_SUCCESS means we have succeeded in making a
+     *    connection. `addr' gives the address we connected to, if
+     *    available. (But sometimes, in cases of complicated proxy
+     *    setups, it might not be available, so receivers of this log
+     *    event should be prepared to deal with addr==NULL.)
      *
      *  - PLUGLOG_PROXY_MSG means that error_msg contains a line of
      *    logging information from whatever the connection is being

be_misc.c
@@ -29,7 +29,10 @@ void backend_socket_log(Seat *seat, LogContext *logctx,
         msg = dupprintf("Failed to connect to %s: %s", addrbuf, error_msg);
         break;
       case PLUGLOG_CONNECT_SUCCESS:
-        sk_getaddr(addr, addrbuf, lenof(addrbuf));
+        if (addr)
+            sk_getaddr(addr, addrbuf, lenof(addrbuf));
+        else /* fallback if address unavailable */
+            sprintf(addrbuf, "remote host");
         msg = dupprintf("Connected to %s", addrbuf);
         break;
       case PLUGLOG_PROXY_MSG: {

proxy.c
@@ -28,6 +28,8 @@ void proxy_activate (ProxySocket *p)
 
     p->state = PROXY_STATE_ACTIVE;
 
+    plug_log(p->plug, PLUGLOG_CONNECT_SUCCESS, NULL, 0, NULL, 0);
+
     /* we want to ignore new receive events until we have sent
      * all of our buffered receive data.
      */

unix/fd-socket.c
@@ -315,6 +315,12 @@ static const SocketVtable FdSocket_sockvt = {
     .peer_info = NULL,
 };
 
+static void fdsocket_connect_success_callback(void *ctx)
+{
+    FdSocket *fds = (FdSocket *)ctx;
+    plug_log(fds->plug, PLUGLOG_CONNECT_SUCCESS, NULL, 0, NULL, 0);
+}
+
 Socket *make_fd_socket(int infd, int outfd, int inerrfd, Plug *plug)
 {
     FdSocket *fds;
@@ -353,6 +359,8 @@ Socket *make_fd_socket(int infd, int outfd, int inerrfd, Plug *plug)
         add234(fdsocket_by_inerrfd, fds);
         uxsel_set(fds->inerrfd, SELECT_R, fdsocket_select_result_input_error);
     }
+
+    queue_toplevel_callback(fdsocket_connect_success_callback, fds);
 
     return &fds->sock;
 }

windows/handle-socket.c
@@ -314,6 +314,12 @@ static const SocketVtable HandleSocket_sockvt = {
     .peer_info = sk_handle_peer_info,
 };
 
+static void sk_handle_connect_success_callback(void *ctx)
+{
+    HandleSocket *hs = (HandleSocket *)ctx;
+    plug_log(hs->plug, PLUGLOG_CONNECT_SUCCESS, NULL, 0, NULL, 0);
+}
+
 Socket *make_handle_socket(HANDLE send_H, HANDLE recv_H, HANDLE stderr_H,
                            Plug *plug, bool overlapped)
 {
@@ -338,6 +344,8 @@ Socket *make_handle_socket(HANDLE send_H, HANDLE recv_H, HANDLE stderr_H,
                                         hs, flags);
 
     hs->defer_close = hs->deferred_close = false;
+
+    queue_toplevel_callback(sk_handle_connect_success_callback, hs);
 
     return &hs->sock;
 }

commit 99b4229abf01ea9b9ef916985e6139feed0c0bf2

Make all Plugs have a log function, even if no-op.
Commit 8f5e9a4f8dc796a introduced a segfault into both Windows Pageant
and Windows connection sharing upstreams when they receive an incoming
named-pipe connection. This occurs because the PlugVtables for those
incoming connections had a null pointer in the 'log' field, because
hitherto, only sockets involved with an outgoing connection expected
to receive plug_log() notifications. But I added such a notification
in make_handle_socket, forgetting that that function is used for both
outgoing and incoming named-pipe connections (among other things). So
now a Plug implementation that expects to be set up by the
plug_accepting() method on a listener may still receive
PLUGLOG_CONNECT_SUCCESS.

I could fix that by adding a parameter to make_handle_socket telling
it whether to send a notification, but that seems like more faff than
is really needed. Simpler to make a rule that says _all_ Socket types
must implement the log() method, even if only with a no-op function.

We already have a no-op implementation of log(), in nullplug.c. So
I've exposed that outside its module (in the same style as all the
nullseat functions and so on), to make it really easy to add log()
methods to PlugVtables that don't need one.

network.h
@@ -78,6 +78,13 @@ struct PlugVtable {
      *    proxied through. This will typically be a wodge of
      *    standard-error output from a local proxy command, so the
      *    receiver should probably prefix it to indicate this.
+     *
+     * Note that sometimes log messages may be sent even to Socket
+     * types that don't involve making an outgoing connection, e.g.
+     * because the same core implementation (such as Windows handle
+     * sockets) is shared between listening and connecting sockets. So
+     * all Plugs must implement this method, even if only to ignore
+     * the logged events.
      */
     void (*closing)
      (Plug *p, const char *error_msg, int error_code, bool calling_back);
@@ -324,6 +331,20 @@ Socket *new_error_socket_consume_string(Plug *plug, char *errmsg);
  * Trivial plug that does absolutely nothing. Found in nullplug.c.
  */
 extern Plug *const nullplug;
+
+/*
+ * Some trivial no-op plug functions, also in nullplug.c; exposed here
+ * so that other Plug implementations can use them too.
+ *
+ * In particular, nullplug_log is useful to Plugs that don't need to
+ * worry about logging.
+ */
+void nullplug_log(Plug *plug, PlugLogType type, SockAddr *addr,
+                  int port, const char *err_msg, int err_code);
+void nullplug_closing(Plug *plug, const char *error_msg, int error_code,
+                      bool calling_back);
+void nullplug_receive(Plug *plug, int urgent, const char *data, size_t len);
+void nullplug_sent(Plug *plug, size_t bufsize);
 
 /* ----------------------------------------------------------------------
  * Functions defined outside the network code, which have to be

nullplug.c
@@ -7,27 +7,26 @@
 
 #include "putty.h"
 
-static void nullplug_socket_log(Plug *plug, PlugLogType type, SockAddr *addr,
-                                int port, const char *err_msg, int err_code)
+void nullplug_log(Plug *plug, PlugLogType type, SockAddr *addr,
+                  int port, const char *err_msg, int err_code)
 {
 }
 
-static void nullplug_closing(Plug *plug, const char *error_msg, int error_code,
-                             bool calling_back)
+void nullplug_closing(Plug *plug, const char *error_msg, int error_code,
+                      bool calling_back)
 {
 }
 
-static void nullplug_receive(
-    Plug *plug, int urgent, const char *data, size_t len)
+void nullplug_receive(Plug *plug, int urgent, const char *data, size_t len)
 {
 }
 
-static void nullplug_sent(Plug *plug, size_t bufsize)
+void nullplug_sent(Plug *plug, size_t bufsize)
 {
 }
 
 static const PlugVtable nullplug_plugvt = {
-    .log = nullplug_socket_log,
+    .log = nullplug_log,
     .closing = nullplug_closing,
     .receive = nullplug_receive,
     .sent = nullplug_sent,

pageant.c
@@ -1624,6 +1624,7 @@ static const PlugVtable pageant_connection_plugvt = {
     .closing = pageant_conn_closing,
     .receive = pageant_conn_receive,
     .sent = pageant_conn_sent,
+    .log = nullplug_log,
 };
 
 static int pageant_listen_accepting(Plug *plug,
@@ -1672,6 +1673,7 @@ static int pageant_listen_accepting(Plug *plug,
 static const PlugVtable pageant_listener_plugvt = {
     .closing = pageant_listen_closing,
     .accepting = pageant_listen_accepting,
+    .log = nullplug_log,
 };
 
 struct pageant_listen_state *pageant_listener_new(

ssh/sharing.c
@@ -1905,6 +1905,7 @@ static const PlugVtable ssh_sharing_conn_plugvt = {
     .closing = share_closing,
     .receive = share_receive,
     .sent = share_sent,
+    .log = nullplug_log,
 };
 
 static int share_listen_accepting(Plug *plug,
@@ -2046,6 +2047,7 @@ bool ssh_share_test_for_upstream(const char *host, int port, Conf *conf)
 static const PlugVtable ssh_sharing_listen_plugvt = {
     .closing = share_listen_closing,
     .accepting = share_listen_accepting,
+    .log = nullplug_log,
 };
 
 void ssh_connshare_provide_connlayer(ssh_sharing_state *sharestate,