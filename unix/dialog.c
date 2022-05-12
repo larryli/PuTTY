larryli/PuTTY

commit 5390aef3fc7deca225dd76db15021dc5c4ab53f2

GTK: make explicit text controls selectable.
This doesn't apply to every GtkLabel I instantiate: only the ones
constructed as part of implementing the cross-platform CTRL_TEXT.
Those labels contain information that the dialog box is deliberately
communicating to the user, so it seems a sensible idea to make sure
they can be copy-pasted.

By default, this also seems to cause them to become able to take the
input focus, so I've reverted that. You can select them with the
mouse, but I think having them appear in the tab order is an
awkwardness too far, since they're not active in any other way.

unix/dialog.c
@@ -2506,6 +2506,8 @@ GtkWidget *layout_ctrls(
              */
             uc->text = w = gtk_label_new(uc->ctrl->label);
 #endif
+            gtk_label_set_selectable(GTK_LABEL(w), true);
+            gtk_widget_set_can_focus(w, false);
             align_label_left(GTK_LABEL(w));
             gtk_label_set_line_wrap(GTK_LABEL(w), true);
             break;

commit 386b094e3f6351d4c81eaffa3e7f385291ae5fad

Fix GTK1 build.
Commit 5390aef3fc7deca broke it, because GTK1 has neither
gtk_label_set_selectable nor gtk_widget_set_can_focus. Happily, those
are both more or less optional (only a minor UI awkwardness arises
from not having them), so I'll just condition them out.

unix/dialog.c
@@ -2506,8 +2506,10 @@ GtkWidget *layout_ctrls(
              */
             uc->text = w = gtk_label_new(uc->ctrl->label);
 #endif
+#if GTK_CHECK_VERSION(2,0,0)
             gtk_label_set_selectable(GTK_LABEL(w), true);
             gtk_widget_set_can_focus(w, false);
+#endif
             align_label_left(GTK_LABEL(w));
             gtk_label_set_line_wrap(GTK_LABEL(w), ctrl->text.wrap);
             if (!ctrl->text.wrap) {
@@ -2518,7 +2520,9 @@ GtkWidget *layout_ctrls(
                 gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(w),
                                                GTK_POLICY_AUTOMATIC,
                                                GTK_POLICY_NEVER);
+#if GTK_CHECK_VERSION(2,0,0)
                 gtk_widget_set_can_focus(w, false);
+#endif
             }
             break;
         }