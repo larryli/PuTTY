larryli/PuTTY

commit d163b37cafc46ab24412e4cf92d14f54e2aa97a6

Squash NDEBUG for RelWithDebInfo build type too.

cmake/setup.cmake
@@ -4,6 +4,8 @@
 # give a #error if this manoeuvre doesn't do what it needs to.
 string(REPLACE "/DNDEBUG" "" CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE}")
 string(REPLACE "-DNDEBUG" "" CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE}")
+string(REPLACE "/DNDEBUG" "" CMAKE_C_FLAGS_RELWITHDEBINFO "${CMAKE_C_FLAGS_RELEASE}")
+string(REPLACE "-DNDEBUG" "" CMAKE_C_FLAGS_RELWITHDEBINFO "${CMAKE_C_FLAGS_RELEASE}")
 
 set(PUTTY_IPV6 ON
   CACHE BOOL "Build PuTTY with IPv6 support if possible")

commit 02c745e1860f593f993fd141938fd53f90327be1

Use correct flags for RelWithDebInfo.
Bug introduced in d163b37caf. (Ahem.)

cmake/setup.cmake
@@ -4,8 +4,8 @@
 # give a #error if this manoeuvre doesn't do what it needs to.
 string(REPLACE "/DNDEBUG" "" CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE}")
 string(REPLACE "-DNDEBUG" "" CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE}")
-string(REPLACE "/DNDEBUG" "" CMAKE_C_FLAGS_RELWITHDEBINFO "${CMAKE_C_FLAGS_RELEASE}")
-string(REPLACE "-DNDEBUG" "" CMAKE_C_FLAGS_RELWITHDEBINFO "${CMAKE_C_FLAGS_RELEASE}")
+string(REPLACE "/DNDEBUG" "" CMAKE_C_FLAGS_RELWITHDEBINFO "${CMAKE_C_FLAGS_RELWITHDEBINFO}")
+string(REPLACE "-DNDEBUG" "" CMAKE_C_FLAGS_RELWITHDEBINFO "${CMAKE_C_FLAGS_RELWITHDEBINFO}")
 
 set(PUTTY_IPV6 ON
   CACHE BOOL "Build PuTTY with IPv6 support if possible")